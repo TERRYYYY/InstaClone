684b96c1e98fc00406e8158e93fd0d46b8135f7b
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-28 15:58:38 +0300

    creating my first template

--- insta/views.py
+++ insta/views.py
@@ -4,4 +4,4 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'welcome.html')
