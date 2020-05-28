defd77b6af7761b5c0d00b37db206e6c161ac015
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-28 16:07:34 +0300

    applying template inheritance

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse, Http404
+import datetime as dt
 
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def intro(request):
+    date = dt.date.today()
+    return render(request, 'all-insta/intro.html', {"date": date,})
