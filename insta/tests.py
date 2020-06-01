2cf3a45f6b7abd42e5c57769e4a0cccc5fa46781
TERRYYYY <kariuki.terry11@gmail.com>
2020-06-01 15:54:31 +0300

    testing django apps

--- insta/tests.py
+++ insta/tests.py
@@ -1,14 +1,14 @@
 from django.test import TestCase
-# from .models import Image,Profile,Comment
-# from django.contrib.auth.models import User
+from .models import Image,Profile,Comment
+from django.contrib.auth.models import User
 
-# # Create your tests here.
-# class ImageTestCase(TestCase):
-#     def setUp(self):
-#         self.image= Image(image_name = 'Beagle', caption ='Lovely dog',pub_date ='2020-06-01', likes='20', image_file ='images/beagle.jpg')
-#         self.image.save_image()
+# Create your tests here.
+class ImageTestCase(TestCase):
+    def setUp(self):
+        self.image= Image(image_name = 'Beagle', caption ='Lovely dog',pub_date ='2020-06-01', likes='20', image_file ='images/beagle.jpg')
+        self.image.save_image()
 
-#     def test_save_images(self):
-#         self.image.save_image()
-#         all_images = Image.objects.all()
-#         self.assertTrue(len(all_images) > 0)
+    def test_save_images(self):
+        self.image.save_image()
+        all_images = Image.objects.all()
+        self.assertTrue(len(all_images) > 0)
